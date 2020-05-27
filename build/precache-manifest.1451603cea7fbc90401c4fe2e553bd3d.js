581e9475a63e1ee5ed8a0b7abb40a370b42cc546
İrem Kaya <38073758+iiremkayaa@users.noreply.github.com>
2020-05-27 23:14:03 +0300

    restructed project

---
+++ build/precache-manifest.1451603cea7fbc90401c4fe2e553bd3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96236e7d75a399145e31482a84c02c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81378ae03591e51ef065",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "012fb2355c91e21a5fa8",
+    "url": "/static/js/2.fec49034.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fec49034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81378ae03591e51ef065",
+    "url": "/static/js/main.97a64dbf.chunk.js"
+  },
+  {
+    "revision": "15b954a92e214f898f73",
+    "url": "/static/js/runtime-main.e13c14c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
