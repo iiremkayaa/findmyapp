ab3f0b1b1d96964337494ff21d33a359e8b372e3
İrem Kaya <38073758+iiremkayaa@users.noreply.github.com>
2020-05-30 15:42:17 +0300

    added password control

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -8,52 +8,80 @@ import * as firebase from "firebase";
 const Register = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [username,setUsername]=useState("");
-    const submit= (event)=>{
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [charControl, setCharControl] = useState(false);
+    const [passControl, setPassControl] = useState(false);
+    const submit = (event) => {
         event.preventDefault();
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
             var errorCode = error.code;
             var errorMessage = error.message;
-          });
+        });
     }
-    const handleEmail =(event)=>{
+    const handleEmail = (event) => {
         setEmail(event.target.value);
     }
-    const handlePassword = (event)=>{
+    const handlePassword = (event) => {
         setPassword(event.target.value);
+        if ((event.target.value.length > 0) && (event.target.value.length < 5)) {
+            setCharControl(true);
+        }
+        else {
+            setCharControl(false);
+        }
+        if (confirmPassword !== event.target.value) {
+            setPassControl(true);
+        }
+        if (confirmPassword === event.target.value) {
+            setPassControl(false);
+        }
     }
-    const handleConfirmPassword = (event)=>{
-        setPassword(event.target.value);
+    const handleConfirmPassword = (event) => {
+        setConfirmPassword(event.target.value);
+        if (password !== event.target.value) {
+            setPassControl(true);
+        }
+        if (password === event.target.value) {
+            setPassControl(false);
+        }
     }
-    const handleUsername =(event)=>{
+    const handleUsername = (event) => {
         setUsername(event.target.value);
     }
     return (
         <div >
             <div style={{ margin: "20%", marginTop: "30px", marginBottom: "10px" }}>
                 <Form>
-                <Form.Group>
+                    <Form.Group >
                         <Form.Label>Username</Form.Label>
                         <Form.Control type="text" placeholder="" onChange={handleUsername} />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="" onChange={handleEmail} />
-                        <Form.Text  style={{color:"white"}}>
+                        <Form.Text style={{ color: "white" }}>
                             We'll never share your email with anyone else.
                         </Form.Text>
                     </Form.Group>
+
                     <Form.Group >
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="" onChange={handlePassword} />
+                        {charControl && <Form.Text style={{ color: "red" }}>
+                            The password field must be at least 5 characters.
+                        </Form.Text>}
                     </Form.Group>
                     <Form.Group >
                         <Form.Label>Confirm Password</Form.Label>
                         <Form.Control type="password" placeholder="" onChange={handleConfirmPassword} />
+                        {passControl && <Form.Text style={{ color: "red" }}>
+                            The password must match confirm password.
+                        </Form.Text>}
                     </Form.Group>
-                    <div  style={{textAlign:"center"}}>
-                    <Button variant="primary" type="submit" onClick={submit}>
-                        Submit
+                    <div style={{ textAlign: "center" }}>
+                        <Button variant="link" type="submit" onClick={submit} style={{ color: "white" }}>
+                            Sign Up
                     </Button>
                     </div>
                 </Form>
