7a2e2bb7721aadf9f472406961a0384850d4ff6b
İrem Kaya <38073758+iiremkayaa@users.noreply.github.com>
2020-06-01 14:56:29 +0300

    added store and payment

--- src/components/Sharing.js
+++ src/components/Sharing.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import Form from 'react-bootstrap/Form';
 import { db } from '../firebase/index';
 import * as firebase from "firebase";
 import FormControl from '@material-ui/core/FormControl';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
+const useStyles = makeStyles((theme) => ({
+	formControl: {
+		margin: theme.spacing(1),
 
+		minWidth: 120,
+	},
+}));
 const Sharing = () => {
 
 	const [description, setDescription] = useState('');
 	const [username, setUsername] = useState("");
 	const [isAnon, setIsAnon] = useState(false);
+	const [store,setStore]=useState("AppStore");
+	const [payment,setPayment]=useState("Free");
 	useEffect(() => {
 		firebase.auth().onAuthStateChanged(userAuth => {
 			db.ref('/user').on('value', querySnapShot => {
@@ -40,9 +49,11 @@ const Sharing = () => {
 		const data = {
 			user: username,
 			description: description,
-			date: convertDateFormatToPost(date), //todo:date will be added
+			date: convertDateFormatToPost(date), 
 			time: time,
-			isAnon: isAnon
+			isAnon: isAnon,
+			store:store,
+			payment:payment,
 		}
 		db.ref('/sharing').push(data);
 		setIsAnon(false);
@@ -52,12 +63,13 @@ const Sharing = () => {
 		setDescription(event.target.value);
 	}
 	const handleSelectStore = (event) => {
-		console.log(event.target.value);
+		setStore(event.target.value);
 	}
 	const handleSelectPayment = (event) => {
-		console.log(event.target.value);
+		setPayment(event.target.value);
 
 	}
+	const classes = useStyles();
 	return (
 		<div style={{ padding: "30px", borderRadius: "5px", borderColor: "white", border: "solid", borderWidth: "1px", backgroundColor: "#6F90AF" }} >
 			<Form style={{ textAlign: "center" }}>
@@ -65,14 +77,14 @@ const Sharing = () => {
 					<Form.Label style={{ fontSize: "20px", padding: "5px" }}>Please provide information of application you are looking for... </Form.Label>
 					<Form.Control required as="textarea" rows="3" value={description} onChange={sharingChange} />
 				</div>
-				</Form>
-				<div style={{ textAlign: "center", paddingTop: "20px" }}>
-					<div style={{ display: "inline", paddingRight: "20px"}}>
-					<FormControl variant="filled" styles={{ minWidth: 120, maxHeight: 30, }} >
+			</Form>
+			<div style={{ textAlign: "center", paddingTop: "20px" }}>
+				<div style={{ display: "inline", paddingRight: "20px" }}>
+					<FormControl variant="filled" className={classes.formControl} >
 						<Select
 							labelId="demo-simple-select-filled-label"
 							style={{ color: 'white', minWidth: 160, maxHeight: 50 }}
-							value={"AppStore"}
+							value={store}
 							onChange={event => { handleSelectStore(event); }}
 						>
 							<MenuItem style={{ color: '#152c3b' }} value={"AppStore"}> App Store</MenuItem>
@@ -80,13 +92,13 @@ const Sharing = () => {
 
 						</Select>
 					</FormControl>
-					</div>
-					<div style={{display:"inline", paddingLeft: "20px" }}>
-					<FormControl variant="filled" styles={{ minWidth: 120, maxHeight: 30}} >
+				</div>
+				<div style={{ display: "inline", paddingLeft: "20px" }}>
+					<FormControl variant="filled" className={classes.formControl} >
 						<Select
 							labelId="demo-simple-select-filled-label"
 							style={{ color: 'white', minWidth: 160, maxHeight: 50 }}
-							value={"Free"}
+							value={payment}
 							onChange={event => { handleSelectPayment(event); }}
 						>
 							<MenuItem style={{ color: '#152c3b' }} value={"Free"}> Free App</MenuItem>
@@ -94,14 +106,14 @@ const Sharing = () => {
 							<MenuItem style={{ color: '#152c3b' }} value={"Both"}> Both</MenuItem>
 						</Select>
 					</FormControl>
-					</div>
 				</div>
-				<div style={{ textAlign: "center", paddingTop: "20px"  }}>
-					<Form>
-					<Form.Check id="custom-switch" type="switch" onChange={onSwitchSharing} label="Anonymous" style={{ fontSize: "18px"}} />
-					</Form>
-				</div>
-			
+			</div>
+			<div style={{ textAlign: "center", paddingTop: "20px" }}>
+				<Form>
+					<Form.Check id="custom-switch" type="switch" onChange={onSwitchSharing} label="Anonymous" style={{ fontSize: "18px" }} />
+				</Form>
+			</div>
+
 			<div style={{ textAlign: "center", width: "100%", marginTop: "30px" }}>
 				<button type="submit" onClick={event => submitSharing(event)} style={{ fontWeight: "400", fontSize: "18px", borderRadius: "8px", color: "white", backgroundColor: "#6F90AF", padding: "5px", paddingLeft: "25px", paddingRight: "25px" }}>
 					SEND
